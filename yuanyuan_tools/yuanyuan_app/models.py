d6c87f576235e8bec6bb690f753c2b4c0d1aa0c7
jiweiling <weiling.ji@shanbay.com>
2018-02-23 16:12:11 +0800

    finish serializers

--- yuanyuan_tools/yuanyuan_app/models.py
+++ yuanyuan_tools/yuanyuan_app/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 """
 MODEL
@@ -27,14 +28,13 @@ GENDER_TYPE = (
 
 class WechatUsers(models.Model):
     nickname = models.CharField(max_length=45)
-    avatar_url = models.CharField(max_length=2000)
     gender = models.SmallIntegerField(GENDER_TYPE, default=GENDER_TYPE[0])
     city = models.CharField(max_length=45)
     province = models.CharField(max_length=45)
     country = models.CharField(max_length=45)
     language = models.CharField(max_length=45)
-    create_time = models.DateTimeField()
-    update_time = models.DateTimeField()
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'wechat_users'
@@ -45,11 +45,11 @@ class Users(models.Model):
     wechat_user_id = models.ForeignKey(WechatUsers, on_delete=True)
     name = models.CharField(max_length=45)
     birthday_type = models.SmallIntegerField(BIRTHDAY_TYPE, default=BIRTHDAY_TYPE[0])
-    birthday = models.DateTimeField()
-    avatar_url = models.CharField(max_length=45)
-    phone_number = models.CharField(max_length=45)
-    create_time = models.DateTimeField()
-    update_time = models.DateTimeField()
+    birthday = models.DateTimeField(null=True)
+    avatar_name = models.CharField(max_length=45)
+    phone_number = models.CharField(null=True, max_length=45)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'users'
@@ -61,10 +61,10 @@ class BirthdayLogs(models.Model):
     name = models.CharField(max_length=45)
     birthday_type = models.SmallIntegerField(BIRTHDAY_TYPE, default=BIRTHDAY_TYPE[0])
     birthday = models.DateTimeField()
-    avatar_url = models.CharField(max_length=45)
-    phone_number = models.CharField(max_length=45)
-    create_time = models.DateTimeField()
-    update_time = models.DateTimeField()
+    avatar_name = models.CharField(null=True, max_length=45)
+    phone_number = models.CharField(null=True, max_length=45)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'birthday_logs'
